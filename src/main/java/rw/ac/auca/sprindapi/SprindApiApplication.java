bfd6fa3af2fa294d07c516462434cb193ba78f6d
la paix <dj.lapaix.tech@gmail.com>
2024-04-04 20:14:06 +0200

    pushing project to git

---
+++ src/main/java/rw/ac/auca/sprindapi/SprindApiApplication.java
@@ -0,0 +1,13 @@
+package rw.ac.auca.sprindapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprindApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprindApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/ac/auca/sprindapi/SprindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.ac.auca.sprindapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprindApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
